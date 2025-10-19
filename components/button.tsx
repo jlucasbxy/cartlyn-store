206a2b442fd888f4337c05872b27f06d7c788d9e
jlucasbx <jlucasb16@gmail.com>
2025-10-19 17:07:09 -0300

    feat(button): greater padding on small size

--- components/button.tsx
+++ components/button.tsx
@@ -16,7 +16,7 @@ const variantClasses = {
 };
 
 const sizeClasses = {
-    sm: 'px-3 py-1.5 text-sm',
+    sm: 'px-4 py-2 text-sm',
     md: 'px-6 py-2 text-base',
     lg: 'px-8 py-3 text-lg',
 };
