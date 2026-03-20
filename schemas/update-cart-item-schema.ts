409d36532a9b45781823574248bef087f186147a
jlucasbx <jlucasb16@gmail.com>
2026-03-19 21:13:39 -0300

    feat: add update-cart-item-schema

---
+++ schemas/update-cart-item-schema.ts
@@ -0,0 +1,7 @@
+import { z } from '@/schemas/zod-config';
+
+export const updateCartItemSchema = z.object({
+    quantity: z.number().int().positive(),
+});
+
+export type UpdateCartItemInput = z.infer<typeof updateCartItemSchema>;
