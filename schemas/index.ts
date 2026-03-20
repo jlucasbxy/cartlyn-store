624c7fa8a1ccb0bcc30578e388033a5f539afeee
jlucasbx <jlucasb16@gmail.com>
2026-03-19 21:13:39 -0300

    feat: add index

---
+++ schemas/index.ts
@@ -0,0 +1,28 @@
+export { registerSchema } from '@/schemas/register-schema';
+export type { RegisterInput } from '@/schemas/register-schema';
+
+export { registerWithConfirmSchema } from '@/schemas/register-with-confirm-schema';
+
+export { loginSchema } from '@/schemas/login-schema';
+export type { LoginInput } from '@/schemas/login-schema';
+
+export { productSchema } from '@/schemas/product-schema';
+export type { ProductInput } from '@/schemas/product-schema';
+
+export { productUpdateSchema } from '@/schemas/product-update-schema';
+export type { ProductUpdateInput } from '@/schemas/product-update-schema';
+
+export { csvProductSchema } from '@/schemas/csv-product-schema';
+export type { CsvProductInput } from '@/schemas/csv-product-schema';
+
+export { addToCartSchema } from '@/schemas/add-to-cart-schema';
+export type { AddToCartInput } from '@/schemas/add-to-cart-schema';
+
+export { updateCartItemSchema } from '@/schemas/update-cart-item-schema';
+export type { UpdateCartItemInput } from '@/schemas/update-cart-item-schema';
+
+export { searchProductsSchema } from '@/schemas/search-products-schema';
+export type { SearchProductsInput } from '@/schemas/search-products-schema';
+
+export { favoriteSchema } from '@/schemas/favorite-schema';
+export type { FavoriteInput } from '@/schemas/favorite-schema';
