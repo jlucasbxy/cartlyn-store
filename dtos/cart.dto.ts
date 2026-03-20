0be641a10c8407ab8fa838acf62d058a66ce7994
jlucasbx <jlucasb16@gmail.com>
2026-03-20 17:11:38 -0300

    feat: add cart dto

---
+++ dtos/cart.dto.ts
@@ -0,0 +1,37 @@
+import type { SellerDTO } from '@/dtos/product.dto';
+
+export type CartProductBaseDTO = {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    imageUrl: string;
+    publishedAt: Date;
+    active: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    sellerId: string;
+};
+
+export type CartProductDTO = CartProductBaseDTO & {
+    seller: SellerDTO;
+};
+
+export type CartItemBaseDTO = {
+    id: string;
+    userId: string;
+    productId: string;
+    quantity: number;
+    createdAt: Date;
+    updatedAt: Date;
+    product: CartProductBaseDTO;
+};
+
+export type CartItemDTO = Omit<CartItemBaseDTO, 'product'> & {
+    product: CartProductDTO;
+};
+
+export type CartDTO = {
+    items: CartItemDTO[];
+    total: number;
+};
