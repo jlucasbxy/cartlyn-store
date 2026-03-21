efb881aed1edb62df22cdedccd21f2e8b0a4c798
jlucasbx <jlucasb16@gmail.com>
2026-03-20 22:31:21 -0300

    feat: add logger utility to lib

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export { auth, authOptions } from "./auth";
 export { formatZodError } from "./format-zod-error";
 export { handleServiceError } from "./handle-service-error";
+export { logger } from "./logger";
 export { toNumber } from "./price";
 export { prisma } from "./prisma";
