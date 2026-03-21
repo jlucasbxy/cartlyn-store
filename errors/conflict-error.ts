91fee5911a46a2c0e37e6723c6ae4d36c96c259d
jlucasbx <jlucasb16@gmail.com>
2026-03-20 22:59:15 -0300

    refactor: remove redundant constructors from ConflictError and NotFoundError

--- errors/conflict-error.ts
+++ errors/conflict-error.ts
@@ -1,10 +1,5 @@
-import type { ErrorCode } from "@/dtos";
 import { DomainError } from "./domain-error";
 
 export class ConflictError extends DomainError {
   public readonly status = 409;
-
-  constructor(code: ErrorCode, message: string) {
-    super(code, message);
-  }
 }
--- errors/not-found-error.ts
+++ errors/not-found-error.ts
@@ -1,10 +1,5 @@
-import type { ErrorCode } from "@/dtos";
 import { DomainError } from "./domain-error";
 
 export class NotFoundError extends DomainError {
   public readonly status = 404;
-
-  constructor(code: ErrorCode, message: string) {
-    super(code, message);
-  }
 }
