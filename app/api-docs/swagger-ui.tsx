5c49116e8f753622b6200d3f949d94fe6e62338b
jlucasbx <jlucasb16@gmail.com>
2026-03-23 13:13:54 -0300

    fix(api-docs): suppress swagger-ui strict mode lifecycle warning

--- app/api-docs/swagger-ui.tsx
+++ app/api-docs/swagger-ui.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
@@ -8,5 +9,31 @@ type Props = {
 };
 
 export default function SwaggerUIComponent({ spec }: Props) {
+  useEffect(() => {
+    const originalConsoleError = console.error;
+
+    console.error = (...args: unknown[]) => {
+      const firstArg = args[0];
+      if (typeof firstArg === "string") {
+        const isUnsafeLifecycleWarning = firstArg.includes(
+          "Using UNSAFE_componentWillReceiveProps in strict mode is not recommended"
+        );
+        const isModelCollapseWarning = firstArg.includes(
+          "Please update the following components: ModelCollapse"
+        );
+
+        if (isUnsafeLifecycleWarning || isModelCollapseWarning) {
+          return;
+        }
+      }
+
+      originalConsoleError(...args);
+    };
+
+    return () => {
+      console.error = originalConsoleError;
+    };
+  }, []);
+
   return <SwaggerUI spec={spec} />;
 }
